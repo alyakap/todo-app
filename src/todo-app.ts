54627482205d372acded30aab112b97d3e1ea160
Alya K. <46496205+alyakap@users.noreply.github.com>
2024-08-26 18:52:12 +0200

    basic app without delete/toggle

--- src/todo-app.ts
+++ src/todo-app.ts
@@ -1,15 +1,44 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
+import '@lion/ui/define/lion-listbox.js';
+import '@lion/ui/define/lion-option.js';
+import '@lion/ui/define/lion-input.js';
 import '@lion/ui/define/lion-button.js';
 
 @customElement('todo-app')
 export class TodoApp extends LitElement {
-  handleClick() {
-    console.log("clicked");
+
+  @property({ type: Array }) todoItems = [
+    { text: 'Wash the dishes', completed: false },
+    { text: 'Buy groceries', completed: false },
+  ];
+
+  _addNewItem(e: Event) {
+    if (e) {
+      e.preventDefault();
+    }
+    const input = this.shadowRoot?.getElementById('addTodoInput') as HTMLInputElement;
+    if (input.value) {
+      const newItem = { text: input.value, completed: false };
+      this.todoItems = [...this.todoItems, newItem];
+      input.value = ''; 
+    }
   }
+
   render() {
     return html`
-        <lion-button @click="${this.handleClick}">click</lion-button>
+
+        <lion-input id="addTodoInput"></lion-input>
+        <lion-button @click="${this._addNewItem}">Add item</lion-button>
+
+        <lion-listbox>
+          ${this.todoItems.map(item => html`
+            <lion-option .choiceValue="${item.text}">
+              ${item.text}
+            </lion-option>
+          `)}
+        </lion-listbox>
+
     `;
   }
 }
